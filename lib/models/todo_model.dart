8d32bb390d9b29a453d23ada63c01b4a3a246bfe
kareem358 <kareemullahafridi10@gmail.com>
2025-07-12 21:11:05 +0500

    Update todo_model.dart

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,7 +1,7 @@
 class ToDoModel{//creating a class of todo model which will
   // will be store in a list form.
-  String title;
-  String desc;
+  final String title;
+  final String desc;
   bool check =false;
   ToDoModel(this.title,
       this.desc,
